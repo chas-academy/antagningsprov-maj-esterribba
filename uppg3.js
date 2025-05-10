acf8ba8c4282f78611573365a9bef25916fdff4f
ester@ribba.se <ester@ribba.se>
2025-05-10 17:09:34 +0200

    Update uppg3.js

--- uppg3.js
+++ uppg3.js
@@ -2,14 +2,12 @@
 
 function uppg3() {
 
-    // Skriv ut summan av två tal
-    // deklarera två variabler, a och b
-    // tilldela värdena 5 och 10 till variablerna
-    // skriv ut summan av a och b i konsolen
-
-
-
+    let a = 5
+    let b = 10
+    let sum = a + b
+    console.log(sum)
 }
+uppg3()
 
 
 module.exports = { uppg3 };
