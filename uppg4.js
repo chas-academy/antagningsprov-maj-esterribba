d8ec76efc026e8ad8b8bfb098b5b016843f30bc8
ester@ribba.se <ester@ribba.se>
2025-05-10 17:11:04 +0200

    Update uppg4.js

--- uppg4.js
+++ uppg4.js
@@ -1,12 +1,13 @@
 
 function uppg4() {
   
-    // deklarera ett objekt med namnet person
-    // objektet ska ha två properties, name och age
-    // tilldela age 25
-    // skriv ut med dotnotation åldern i konsolen
-
-
+    let person = {
+        name: "unknown",
+        age: 25,
+    }
+    console.log(person.age)
+  
 }
+uppg4()
 
 module.exports = { uppg4 };
