220f19b531d9c85b4d50458046ff7f4ecd8b8922
ester@ribba.se <ester@ribba.se>
2025-05-10 17:12:27 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,12 @@
 
-
 function uppg6(){
 
-    // Skapa en array med namn som du döper till names
-    // Loopa igenom arrayen och skriv ut alla namn i konsolen
+    let names = ["Robin", "Emma", "Judith", "Carl"];
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 
- 
 }
+uppg6()
 
 module.exports = { uppg6 };
