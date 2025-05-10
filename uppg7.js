3b247c1e1df795ff413221df9f3d14c0b6465a38
ester@ribba.se <ester@ribba.se>
2025-05-10 17:13:05 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -2,11 +2,12 @@
 
 function uppg7() {
    
-    // skapa en funktion som tar in två tal som argument
-    // funktionen ska returnera summan av talen
-    // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
-
+   function sum (a, b) {
+        return a + b
+   }
+   console.log(sum(5, 10))
  
 }
+uppg7()
 
 module.exports = { uppg7 };
