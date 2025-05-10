1870adc076815e02489837505ce93e0ad431e4b2
ester@ribba.se <ester@ribba.se>
2025-05-10 17:11:52 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -1,11 +1,16 @@
 
 function uppg5() {
-    // skriv en if-sats som jämför två tal
-    // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
-    // annars skriv ut "Det andra talet är större än det första"
+    
+    let a = 12
+    let b = 8
 
-
-
+    if (a > b) {
+        console.log("Det första talet är större än det andra")
+    }
+    else {
+        console.log("Det andra talet är större än det första")
+    }
 }
+uppg5()
 
 module.exports = { uppg5 };
