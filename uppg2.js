fe3c929230161c58b44962fbef1f0e22671531b5
ester@ribba.se <ester@ribba.se>
2025-05-10 17:08:45 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -1,9 +1,9 @@
 
 function uppg2() {
-   // Skriv din kod här
-   // deklarera en variable med ditt namn och skriv ut det i konsolen
 
-
+   let name = "Ester"
+   console.log(name)
 }
+uppg2()
 
 module.exports = { uppg2 };
